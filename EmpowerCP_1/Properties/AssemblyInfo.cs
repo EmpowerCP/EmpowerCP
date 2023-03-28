b14fc444230cf2968ffcf8383f3218fde523c4df
Christian Paske <cpaske@stellar-pcs.com>
2023-03-28 14:25:03 +0200

    Projektdateien hinzufügen.

---
+++ EmpowerCP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerCP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerCP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EFA60E92-4DFF-47A2-AD7F-1C59B8C45CED")]
+[assembly: AssemblyVersion("1.0.0.0")]
